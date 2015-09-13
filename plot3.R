921d74523cb7723a9e556520fe1cc68ca122c8f6
Ellenalexia <ellen.rachel@gmail.com>
2015-09-13 15:25:59 -0700

    plot 3

---
+++ plot3.R
@@ -0,0 +1,25 @@
+## Created for Coursera :: Exploratory Data Analysis::
+## September 2015
+##
+## *** From instructions ***
+## "We will only be using data from the dates 2007-02-01 and 2007-02-02. 
+## One alternative is to read the data from just those dates 
+## rather than reading in the entire dataset and subsetting to those dates."
+## 
+## ** So to create a smaller file with only the dates needed:
+## 1) download entire file
+## 2) at command line, grep ^[1-2]\/2\/2007 [name of entire data file].txt > subset.txt
+## 3) ensure file "subset.txt" is in working directory
+
+powerData <- read.table("subset.txt",header=TRUE,sep=";",dec=".",na.strings=c("?",""))
+names(powerData) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+powerData$Date <- as.Date(powerData$Date, "%d/%m/%Y")
+powerData$Time <- strptime(paste(powerData$Date,powerData$Time),"%Y-%m-%d%T")
+
+png(filename="plot3.png",width=480,heigh=480,bg = "transparent")
+plot(powerData$Time,powerData$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+points(powerData$Time,powerData$Sub_metering_1,type="l",col="black")
+points(powerData$Time,powerData$Sub_metering_2,type="l",col="red")
+points(powerData$Time,powerData$Sub_metering_3,type="l",col="blue")
+legend("topright", pch = c("-","-","-"), col= c("black","blue","red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
